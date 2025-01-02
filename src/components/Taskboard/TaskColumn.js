a12b1f4faf2ab0b96bfa859920fc4e1c1700843b
ishaqsk442 <skishaq456@gmail.com>
2025-01-02 16:46:00 +0530

    done

--- src/components/Taskboard/TaskColumn.js
+++ src/components/Taskboard/TaskColumn.js
@@ -1,51 +1,4 @@
-// import React, { useContext } from "react";
-// import { useDrop } from "react-dnd";
-// import { TaskContext } from "../../contexts/TaskContext";
-// import TaskCard from "./TaskCard";
-// import "./TaskColumn.css"
 
-// const TaskColumn = ({ title, status }) => {
-//   const { tasks, changeStatus,darkMode } = useContext(TaskContext);
- 
-
- 
-//   const filteredTasks = tasks.filter((task) => task.status === status);
-
-//   const [{ isOver }, drop] = useDrop({
-//     accept: "task",
-//     drop: (item) => {
-//       changeStatus(item.id, status);
-//     },
-//     collect: (monitor) => ({
-//       isOver: !!monitor.isOver(),
-//     }),
-//   });
-
-//   return (
-//     <div
-//       ref={drop}
-//       style={{
-//         background: isOver ? "#e0e0e0" : "inherit",   
-//       }}
-//       className="column"
-//     >
-//       <h2>
-//         {title} ({filteredTasks.length} tasks)
-//       </h2>
-//       {filteredTasks.map((task) => (
-//         <TaskCard
-//           key={task.id}
-//           task={task}
-//           mode={darkMode}
-//           updateTask={(id, updatedTask) => changeStatus(id, updatedTask)}
-//           deleteTask={(id) => changeStatus(id, "deleted")}
-//         />
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default TaskColumn;
 import React, { useContext } from "react";
 import { useDrop } from "react-dnd";
 import { TaskContext } from "../../contexts/TaskContext";
@@ -61,7 +14,7 @@ const TaskColumn = ({ title, status }) => {
     accept: "task",
     drop: (item) => {
       if (item.currentStatus !== status) {
-        changeStatus(item.id, status); // Only change status if it's different
+        changeStatus(item.id, status); 
       }
     },
     collect: (monitor) => ({
@@ -85,8 +38,8 @@ const TaskColumn = ({ title, status }) => {
           key={task.id}
           task={task}
           mode={darkMode}
-          updateTask={(updatedTask) => editTask(task.id, updatedTask)} // Pass editTask for modal updates
-          deleteTask={() => removeTask(task.id)} // Use removeTask for deletion
+          updateTask={(updatedTask) => editTask(task.id, updatedTask)}
+          deleteTask={() => removeTask(task.id)} 
         />
       ))}
     </div>
